741036f83a90b7535668ac8e5d9e3ce5e821f3d6
Jason Schramm <jason.schramm@gmail.com>
2025-07-24 21:03:12 -0400

    Add tabs to pages

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Link from 'next/link';
 import ProjectCard from '@/components/ProjectCard'
 import { projects } from '@/lib/project-data'
 
@@ -8,7 +9,10 @@ export default function Projects() {
     <div>
         <Header />
         <div className="flex flex-col">
-            <h1 className="text-3xl font-semibold tracking-wider font-poppins border-b-2 border-gray-400">Projects</h1>
+            <div className="flex text-xl font-semibold tracking-wider font-poppins border-b-2 border-gray-400">
+              <Link href="/" className="tab inactiveTab">About Me</Link>
+              <div className="tab activeTab ml-4">Projects</div>
+            </div>
             <div className="mt-4 flex flex-col space-y-4 font-poppins">
                 { projects.map( (project, i) => (
                     <ProjectCard
