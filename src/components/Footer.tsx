32997c668ba2a196724a2812b4b39390fe0da50e
Jason Schramm <jason.schramm@gmail.com>
2025-08-09 06:00:21 -0400

    Remove header and footer contents

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer className="flex flex-grow mt-8">
-            <a href="mailto:jason.schramm@gmail.com" className="flex flex-1 bg-buttons text-white font-poppins font-semibold tracking-wider rounded-full ring-4 ring-blue-100 py-2" target="_blank">
-                <span className="flex-grow text-center">Email Jason</span>
-            </a>
+		<footer className="flex flex-grow">
         </footer>
 	)
 }
